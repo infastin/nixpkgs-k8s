b6b9da1a00e2fe79cbce4d12972dd2a9dc7bbc95
infastin <infastin@yandex.com>
2025-07-30 22:32:26 +0500

    feat: update kubectl

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -8,33 +8,33 @@
     {
       overlays = {
         default = final: prev: {
-          kubectl_1_31_10 = prev.kubectl.overrideAttrs (oldAttrs: rec {
-            version = "1.31.10";
+          kubectl_1_31_11 = prev.kubectl.overrideAttrs (oldAttrs: rec {
+            version = "1.31.11";
             src = prev.fetchFromGitHub {
               owner = "kubernetes";
               repo = "kubernetes";
               rev = "v${version}";
-              hash = "sha256-renFznJJLYvfIJn59yx9BeZ5HRtSIdtDXXIK2CEs4MU=";
+              hash = "sha256-w4pVk2J7LuU2jbyklmOZSUrB1AwIzusQgxp89OdtK1I=";
             };
           });
 
-          kubectl_1_32_6 = prev.kubectl.overrideAttrs (oldAttrs: rec {
-            version = "1.32.6";
+          kubectl_1_32_7 = prev.kubectl.overrideAttrs (oldAttrs: rec {
+            version = "1.32.7";
             src = prev.fetchFromGitHub {
               owner = "kubernetes";
               repo = "kubernetes";
               rev = "v${version}";
-              hash = "sha256-MCKYcum4rib7qzKyZ7YMMYYhSPqNF5a/D33d9REa8Eo=";
+              hash = "sha256-64002KyKgITobf4WCVgA1TQwJmnIG9rVUuuq8wPYKn4=";
             };
           });
 
-          kubectl_1_33_2 = prev.kubectl.overrideAttrs (oldAttrs: rec {
-            version = "1.33.2";
+          kubectl_1_33_3 = prev.kubectl.overrideAttrs (oldAttrs: rec {
+            version = "1.33.3";
             src = prev.fetchFromGitHub {
               owner = "kubernetes";
               repo = "kubernetes";
               rev = "v${version}";
-              hash = "sha256-Ef/tpjM5RGQzO8rZxTad23DuM6VLlV3N54LOu7dtc6A=";
+              hash = "sha256-UZdrfQEEx0RRe4Bb4EAWcjgCCLq4CJL06HIriYuk1Io=";
             };
           });
         };
@@ -85,9 +85,9 @@
                 };
               };
           in {
-            first = buildImage { kubectl = pkgs.kubectl_1_31_10; };
-            second = buildImage { kubectl = pkgs.kubectl_1_32_6; };
-            third = buildImage { kubectl = pkgs.kubectl_1_33_2; };
+            first = buildImage { kubectl = pkgs.kubectl_1_31_11; };
+            second = buildImage { kubectl = pkgs.kubectl_1_32_7; };
+            third = buildImage { kubectl = pkgs.kubectl_1_33_3; };
           };
       });
 }
